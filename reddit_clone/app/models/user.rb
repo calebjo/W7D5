32f8fdd9a78898bf933422af0a60b04a31282f24
calebjo <caleb.ri.jones@gmail.com>
2021-11-19 13:52:53 -0500

    Finish FIGVAPER methods for user.rb

--- reddit_clone/app/models/user.rb
+++ reddit_clone/app/models/user.rb
@@ -11,27 +11,37 @@ class User < ApplicationRecord
     # FIGVAPER
 
     def self.find_by_credentials(username, password)
-        @user = User.find_by(username: username)
+        user = User.find_by(username: username)
 
-        if @user && @user.is_password?(password)
-            @user
+        if user && user.is_password?(password)
+            user
         else
             nil
         end
     end
 
     def is_password?(password)
+        # if self's pw digest can be converted back into the arg pw, they are the same pw
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
     end
 
     def generate_session_token
+        SecureRandom::urlsafe_base64
     end
 
     def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
     end
 
     def ensure_session_token
+        self.session_token ||= generate_session_token
     end
 
     def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
     end
 end
